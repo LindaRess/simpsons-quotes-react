7b8688aa64d8cf5ffd6af8c0e87bfe075ab189ea
“Linda <linda.resseguier@gmail.com>
2020-10-26 15:37:28 +0100

    quoteform with value character

--- simpsons-quotes-react/src/components/QuoteForm.js
+++ simpsons-quotes-react/src/components/QuoteForm.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './QuoteForm.css';
 
 class QuoteForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {character: 'Homer Simpson',};
+  }
   render() {
     return (
       <form className="QuoteForm" onSubmit={event => event.preventDefault()}>
         <label htmlFor="character">Character:</label>
-        <input
-          id="name"
+        <input 
+          id="name" 
           name="character"
           type="text"
-          value="Homer"
+          value={this.state.character}
           onChange={event => {
             const input = event.target;
             console.log('NAME:', input.name, 'VALUE:', input.value);
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import './QuoteCard.css';
-import PropTypes from 'prop-types';
 
-function QuoteCard(props) {
+class QuoteCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorite: false,
+    };
+  }
+  render() {
     return (
       <figure className="QuoteCard">
-        <img src={props.image} alt={props.character} />
+        <img src={this.props.image} alt={this.props.character} />
         <figcaption>
-          <blockquote>{props.quote}</blockquote>
-          <cite>{props.characterFirstName + ' ' + props.characterLastName}</cite>
+          <blockquote>{this.props.quote}</blockquote>
+          <p>
+            <cite>{this.props.character}</cite>
+            <span
+              className={this.state.favorite ? 'is-favorite' : ''}
+              onClick={event => {
+                const newFavorite = !this.state.favorite;
+                this.setState({ favorite: newFavorite });
+              }}
+            >
+              &#9733;
+            </span>
+          </p>
         </figcaption>
       </figure>
     );
   }
-
-
-QuoteCard.propTypes = {
-  characterFirstName: PropTypes.string.isRequired,
-  characterLastName: PropTypes.string.isRequired,
-};
+}
 
 export default QuoteCard;
